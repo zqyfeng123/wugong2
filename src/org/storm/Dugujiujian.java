a5b5a5dde934cd3a87d9325c2e45dc8dbd09d964
linghuchong <648949867@qq.com>
2020-02-19 20:29:54 +0800

    jie jue

--- src/org/storm/Dugujiujian.java
+++ src/org/storm/Dugujiujian.java
@@ -5,10 +5,7 @@ public class Dugujiujian {
 		System.out.println("1111111111111111");
 		System.out.println("2222222222222222");
 		System.out.println("3333333333333333");
-<<<<<<< HEAD
 		System.out.println("xxxxxxxxxxxxxxx");
-=======
 		System.out.println("yyyyyyyyyyyyyyyy");
->>>>>>> branch 'master' of git@github.com:zqyfeng123/wugong2.git
 	}
 }
